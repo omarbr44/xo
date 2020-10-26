e3ed1ab92b270f3dae6bd2198f5172095dcf647d
omarbr44 <omarbr44@users.noreply.github.com>
2020-10-26 23:47:30 +0300

    hh

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var x=0;
+var x=0,y=0;
 const div=document.querySelectorAll('.div');
 const win=document.querySelector('#win');
 div.forEach(e => {
@@ -6,20 +6,75 @@ div.forEach(e => {
 });   
 
 function turn(ev){
+   console.log(ev.target.innerHTML);
     ev.target.style.background='red';
     x=x+1;
+    let numm= Number.parseInt(ev.target.innerHTML);
+    console.log(numm);
+
     if(x!==5){
-    var num= Math.floor(Math.random() * (8 - 0 + 0)) + 0;
+    var num= Math.floor(Math.random() * (8 - 0 + 0) + 0);
     
+    console.log(num);
 
 
     while(div[num].style.background==="red" ||div[num].style.background==="green"){
-        num= Math.floor(Math.random() * (8 - 0 + 0)) + 0;
-   }        div[num].style.background='green';
-  
+       num= Math.floor(Math.random() * (8 - 0 + 0) + 0);
+   } 
+   if(div[0].style.background==='red' && div[1].style.background==='red' && div[2].style.background!=='green' && div[2].style.background!=='red'){
+        div[2].style.background='green';
+        div[2].removeEventListener('click',turn);}
+        else if(div[3].style.background==='red' && div[4].style.background==='red'&& div[5].style.background!=='green' && div[5].style.background!=='red'){
+        div[5].style.background='green';
+        div[5].removeEventListener('click',turn);}
+        else if(div[5].style.background==='red' && div[4].style.background==='red' && div[3].style.background!=='green' && div[3].style.background!=='red' ){
+        div[3].style.background='green';
+        div[3].removeEventListener('click',turn);}
+        else if(div[2].style.background==='red' && div[1].style.background==='red' && div[0].style.background!=='green' && div[0].style.background!=='red'){
+        div[0].style.background='green';
+        div[0].removeEventListener('click',turn);}
+        else if(div[8].style.background==='red' && div[7].style.background==='red'  && div[6].style.background!=='green' && div[6].style.background!=='red'){
+        div[6].style.background='green';
+        div[6].removeEventListener('click',turn);}
+         else if(div[6].style.background==='red' && div[7].style.background==='red'  && div[8].style.background!=='green' && div[8].style.background!=='red'){
+         div[8].style.background='green';
+         div[8].removeEventListener('click',turn);}
+           else if (div[0].style.background==='red' && div[3].style.background==='red'  && div[6].style.background!=='green' && div[6].style.background!=='red'){
+           div[6].style.background='green';
+           div[6].removeEventListener('click',turn);}
+           else if (div[6].style.background==='red' && div[3].style.background==='red'  && div[0].style.background!=='green' && div[0].style.background!=='red'){
+           div[0].style.background='green';
+           div[0].removeEventListener('click',turn);}
+            else if (div[1].style.background==='red' && div[4].style.background==='red'  && div[7].style.background!=='green' && div[7].style.background!=='red'){
+            div[7].style.background='green';
+            div[7].removeEventListener('click',turn);}
+            else if (div[7].style.background==='red' && div[4].style.background==='red'  && div[1].style.background!=='green' && div[1].style.background!=='red'){
+            div[1].style.background='green';
+            div[1].removeEventListener('click',turn);}
+            else if (div[2].style.background==='red' && div[5].style.background==='red'  && div[8].style.background!=='green' && div[8].style.background!=='red'){
+            div[8].style.background='green';
+            div[8].removeEventListener('click',turn);}
+            else if (div[8].style.background==='red' && div[5].style.background==='red'  && div[2].style.background!=='green' && div[2].style.background!=='red'){
+            div[2].style.background='green';
+            div[2].removeEventListener('click',turn);}
+           else if (div[0].style.background==='red' && div[4].style.background==='red'  && div[8].style.background!=='green' && div[8].style.background!=='red'){
+           div[8].style.background='green';
+           div[8].removeEventListener('click',turn);}
+           else if (div[8].style.background==='red' && div[4].style.background==='red'  && div[0].style.background!=='green' && div[0].style.background!=='red'){
+            div[0].style.background='green';
+            div[0].removeEventListener('click',turn);}
+           else if (div[6].style.background==='red' && div[4].style.background==='red'  && div[2].style.background!=='green' && div[2].style.background!=='red'){
+            div[2].style.background='green';
+            div[2].removeEventListener('click',turn);}
+            else if (div[2].style.background==='red' && div[4].style.background==='red'  && div[6].style.background!=='green' && div[6].style.background!=='red'){
+               div[6].style.background='green';
+               div[6].removeEventListener('click',turn);}
+           else if(y===0){
+           div[num].style.background='green';
+           div[num].removeEventListener('click',turn);}
+
 }  ev.target.removeEventListener('click',turn);
-if(x!==5)
-div[num].removeEventListener('click',turn);
+//if(x!==5)
 if((div[0].style.background==='red' && div[1].style.background==='red' && div[2].style.background==='red') || (div[3].style.background==='red' && div[4].style.background==='red' && div[5].style.background==='red')
   || (div[6].style.background==='red' && div[7].style.background==='red' && div[8].style.background==='red') || (div[0].style.background==='red' && div[3].style.background==='red' && div[6].style.background==='red')
   || (div[1].style.background==='red' && div[4].style.background==='red' && div[7].style.background==='red') || (div[2].style.background==='red' && div[5].style.background==='red' && div[8].style.background==='red')
